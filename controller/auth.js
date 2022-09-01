a26f03ebbb93a142d54b9f6e7c8ee4f37f8deb00
gars150387 <gar.santeliz@gmail.com>
2022-09-01 17:50:54 -0400

    create user and update user info were set up

--- controller/auth.js
+++ controller/auth.js
@@ -1,53 +1,95 @@
-const { response } = require('express')
-const User = require('../models/User')
+const { response } = require("express");
+const User = require("../models/User");
 
-const newUser = async (request, response = response ) => {
+const newUser = async (request, response = response) => {
+  // const { groupName, name, lastName, email, phoneNumber} = request.body
+  const { email } = request.body;
+  try {
+    let user = await User.findOne({ email });
 
-    // const { groupName, name, lastName, email, phoneNumber} = request.body
-
-    try {
-        
-        const user = new User( request.body )
-
-        await user.save()
-        
-        response.status(201).json({
-            ok: true,
-            msg: 'new user',
-            user
-        });
-
-    } catch (error) {
-        console.log( error )
-
-        response.status(500).json({
-            ok: false,
-            msg: 'Please contact adminitrator',
-        });
+    if (user) {
+      return response.status(400).json({
+        ok: false,
+        msg: "Email is registered already",
+      });
     }
 
+    user = new User(request.body);
 
-}
+    await user.save();
 
+    response.status(201).json({
+      ok: true,
+      msg: "new user",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
 
-const login = (request, response = response) => {
+    response.status(500).json({
+      ok: false,
+      msg: "Please contact adminitrator",
+    });
+  }
+};
 
-    console.log(request.body)
-    response.json({
+const editUser = async (request, response = response) => {
+    const userId = request.params.id;
+  
+    console.log({userId});
+  
+    try {
+      const user = await User.findById(userId);
+      console.log({user});
+  
+      if (!user) {
+        return response.status(404).json({
+          ok: false,
+          msg: "User/id do not match",
+        });
+      }
+  
+      // if (event.user.toString() !== uid) {
+      //   return response.status(401).json({
+      //     ok: false,
+      //     msg: "No auhorize to edit event",
+      //   });
+      // }
+  
+      const userEdited = {
+        ...request.body,
+      };
+  
+      const userUpdated = await User.findByIdAndUpdate(userId, userEdited, {
+        new: true,
+        
+      }); //3 parameters user id, new user and how it will be returned as new event
+  
+      console.log({userUpdated})
+      
+      return response.json({
         ok: true,
-        msg: 'login'
-    })
-}
+        user: userUpdated,
+      });
 
+    } catch (error) {
+      console.log(error);
+      return response.status(500).json({
+        ok: false,
+        msg: "Contact Administrator",
+      });
+    }
+  };
+  
 const renewToken = (request, response = response) => {
-    response.json({
-        ok: true,
-        msg: "renew"
-    })
-}
+  response.json({
+    ok: true,
+    msg: "renew",
+  });
+};
 
 module.exports = {
-    newUser,
-    renewToken,
-    login
-}
+  newUser,
+  renewToken,
+  editUser,
+};
